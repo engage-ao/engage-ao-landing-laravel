4b9e3c2deadde8a48064b01f2a6abec958636172
Monteiro Futila <jonquerfutila@gmail.com>
2025-04-15 16:57:41 +0100

    ajuste do copywriter na pagina das marcas

--- resources/views/components/marcas/beta-section.blade.php
+++ resources/views/components/marcas/beta-section.blade.php
@@ -3,13 +3,12 @@
     <div class="container mx-auto flex flex-col items-center justify-between">
         <div class="w-full flex flex-col justify-between max-md:max-w-full">
             <div class="text-[rgba(38,45,61,1)] text-4xl font-Kufam font-normal text-center max-md:max-w-full">
-                Fique por dentro de tudo sobre a ENGAGE
+                Fique por dentro da ENGAGE em primeira mão
             </div>
             <div class="text-[rgba(38,45,61,1)] font-Kufam text-xl font-normal text-center mt-[17px] max-md:max-w-full">
-                O lançameto está previsto para
-                <span class="font-bold">dia 25 de Janeiro de 2025</span>,
-                <br class="max-md:hidden" /> para a versão beta
-                <span class="font-black"> apenas 25 marcas vão poder testar</span>
+                Lançamento exclusivo no dia <span class="font-bold">25 de Janeiro de 2025 </span>.
+                <br class="max-md:hidden" /> Apenas <span class="font-black">25 marcas</span>
+                terão acesso antecipado à versão BETA. Esta é a sua oportunidade única!
             </div>
         </div>
         <div class="w-full flex justify-center mt-[58px] max-md:max-w-full max-md:mt-10">
@@ -19,13 +18,11 @@
             <div
                 class="lg:-ml-10 max-w-md bg-white shadow-[2px_1px_2px_rgba(0,0,0,0.23)] border flex flex-col text-sm font-normal p-10 rounded-2xl border-[rgba(155,100,243,0.66)] border-solid max-md:max-w-full">
                 <div class="text-[rgba(38,45,61,1)] font-Kufam text-[32px] font-medium">
-                    Garante o lugar da sua
-                    <br />
-                    empresa
+                    Garanta agora o lugar da sua empresa
                 </div>
                 <div class="text-[rgba(38,45,61,1)] font-Inter text-sm font-medium mt-[20px]">
-                    Preencha o formulário e estege entre as 25 marcas que terão o
-                    privilegio de testar a versão BETA
+                    Preencha o formulário abaixo e assegure seu acesso privilegiado, sem custos e sem
+                    riscos. Não deixe essa oportunidade escapar!
                 </div>
                 <form class="w-full" method="POST" action="{{ route('marcas.store') }}">
                     @csrf
@@ -51,7 +48,7 @@
                         <label for="phone"
                             class="text-[rgba(88,88,88,1)] font-Kufam text-base font-normal">Telefone</label>
                         <input type="hidden" name="country_code" id="country_code" value="{{ old('country_code') }}" />
-                        <input type="tel" id="phone" name="phone" value="{{ old('phone') }}"
+                        <input type="tel" id="phone" name="phone" placeholder="9XX XXX XXX" value="{{ old('phone') }}"
                             class="bg-[rgba(217,217,217,0.08)] border text-[rgba(88,88,88,1)] w-full px-[13px] py-4 rounded-[9px] border-[rgba(88,88,88,0.32)] border-solid" />
                         @error('phone')
                             <div class="text-red-500 text-sm">{{ $message }}</div>
@@ -80,7 +77,7 @@
                     </div>
                     <button type="submit"
                         class="bg-[rgba(125,75,206,1)] w-full mt-[30px] min-h-[63px] font-Kufam text-base font-medium text-white rounded-[12px] uppercase text-center cursor-pointer hover:bg-[rgba(125,75,206,0.9)] transition-colors">
-                        Cadastrar-se
+                        Garantir meu acesso
                     </button>
                 </form>
             </div>
